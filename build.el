c8507118492d1ac20c84be30108f3b24149caf2b
Nic Ferrier <nic@ferrier.me.uk>
2012-03-30 20:12:25 +0200

    build file and fix to the footer

---
+++ build.el
@@ -0,0 +1,36 @@
+;; Batch run stuff
+
+(setq debug-on-error t)
+
+(setq package-dir
+      (concat
+       (file-name-directory
+        (or (buffer-file-name)
+            load-file-name))
+       "elpa"))
+
+(setq package-user-dir package-dir)
+
+(when (file-exists-p package-dir)
+  (delete-directory package-dir t))
+
+(setq package-archives
+      '(("localcache" . "/home/nferrier/elpacache")
+	;;("gnu" . "http://elpa.gnu.org/packages/")
+        ;;("marmalade" . "http://marmalade-repo.org/packages/"))
+	))
+
+(package-initialize)
+(package-refresh-contents)
+
+(let ((nicblogmake (concat
+		    (file-name-directory
+		     (or (buffer-file-name)
+			 load-file-name))
+		    "nicblog-make.el")))
+  (package-install-file nicblogmake))
+
+(require 'nicblog-make)
+(nicblog-make-run (nicblog-make-root-maker "blog"))
+
+;; End
